82bddd01b02f340a8ee83c42d37e2a4318aec9b7
arseniy200620 <brodnikov2006@gmail.com>
2020-07-05 18:29:24 +0400

    Converter test

---
+++ src/main/java/MyHomeStudy/MyHomeStudy/converter/ConverterTest.java
@@ -0,0 +1,19 @@
+package MyHomeStudy.MyHomeStudy.converter;
+
+import static MyHomeStudy.MyHomeStudy.converter.Converter.rubleToDollar;
+import static MyHomeStudy.MyHomeStudy.converter.Converter.rubleToEuro;
+
+public class ConverterTest {
+    public static void main(String[] args) {
+        int in = 140;
+        int expected = 2;
+        int out = rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+        int ind = 120;
+        int expectedd = 2;
+        int outd = rubleToDollar(ind);
+        boolean passedD = expectedd == outd;
+        System.out.println("120 rubles are 2. Test result : " + passedD);
+    }
+}
