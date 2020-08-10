f26a1af33b5cf15b38c923ff407a479e734b83fd
Arseniy200620 <brodnikov2006@gmail.com>
2020-08-10 19:06:19 +0400

    MHS

--- src/main/java/MyHomeStudy/MyHomeStudy/AlertDivByZero/possibleDiv.java
+++ src/main/java/MyHomeStudy/MyHomeStudy/AlertDivByZero/possibleDiv.java
@@ -8,13 +8,10 @@ class AlertDivByZero {
     }
 
     public static void possibleDiv(int number) {
-        if (number <= 0)
+        if (number < 0) {
             System.out.println("This is negative numbers");
-        else
-                System.out.println("Could not div by 0.");
-
-
-
-
+        }else if (number == 0) {
+            System.out.println("Could not div by 0.");
+        }
     }
 }
