1074c5eeb047a87f7e716dd748077f84e676b23b
Arseniy200620 <brodnikov2006@gmail.com>
2020-09-28 12:10:58 +0400

    MHS

--- src/main/java/MyHomeStudy/MyHomeStudy/MilitiMax.java
+++ src/main/java/MyHomeStudy/MyHomeStudy/MilitiMax.java
@@ -1,8 +1,7 @@
 package MyHomeStudy.MyHomeStudy;
 public class MilitiMax {
     public static int max(int first, int second, int third) {
-        boolean a = (first > second ? first:second) > third;
-        int result = a ? (first > second ? first:second):third;
-        return result;
+        int result = first > second ? first:second;
+        return result > third ? result : third;
     }
 }
