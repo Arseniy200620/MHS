7a2e68389b6595bffab71f093a97c3019d1eed52
Arseniy200620 <brodnikov2006@gmail.com>
2020-10-04 12:50:47 +0400

    MHS

---
+++ src/main/java/MyHomeStudy/MyHomeStudy/Board.java
@@ -0,0 +1,24 @@
+package MyHomeStudy.MyHomeStudy;
+
+public class Board {
+    public static void paint(int width, int height) {
+        for ( int indexW = 1; indexW <= width; indexW++  ) {
+            for ( int indexH = 1; indexH <= height; indexH++ ) {
+                int sum = indexH + indexW;
+
+                if (sum % 2 == 0) {
+                    System.out.print("X ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            /* добавляем перевод на новую строку. */
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        paint(3, 3);
+        System.out.println();
+        paint(4, 4);
+    }
+}
