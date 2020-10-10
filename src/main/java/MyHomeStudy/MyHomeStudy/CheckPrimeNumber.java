00e43fcd83248758e4e77ca596ca9faef27c7283
Arseniy200620 <brodnikov2006@gmail.com>
2020-10-10 21:49:11 +0400

    MHS

--- src/main/java/MyHomeStudy/MyHomeStudy/CheckPrimeNumber.java
+++ src/main/java/MyHomeStudy/MyHomeStudy/CheckPrimeNumber.java
@@ -4,13 +4,10 @@ public class CheckPrimeNumber {
     public static boolean check(int number) {
         boolean prime = true;
         for (int a = 2; (number % a) == 0; a++) {
-       if (a < number) {
-           prime = false;
-           break;
-       }  else  {
-           prime = true;
-           break;
-       }
+       if (a <= (number / 2)) {
+                prime = false;
+                break;
+           }
         }
         return prime;
 
