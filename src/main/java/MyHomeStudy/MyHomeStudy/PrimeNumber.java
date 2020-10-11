7f4ed2b570d12b8802dce878fabbeef0658ed68f
Arseniy200620 <brodnikov2006@gmail.com>
2020-10-11 20:02:14 +0400

    MHS

---
+++ src/main/java/MyHomeStudy/MyHomeStudy/PrimeNumber.java
@@ -0,0 +1,13 @@
+package MyHomeStudy.MyHomeStudy;
+
+public class PrimeNumber {
+    public static int calc(int finish) {
+        int count = 0;
+        for (int number = 2; number <= finish; number++) {
+            if (CheckPrimeNumber.check(number) == true) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
