b7dd8a24932310bca9cad28b6edee34511d074a2
Arseniy200620 <brodnikov2006@gmail.com>
2020-10-28 13:30:28 +0400

    MHS

---
+++ src/main/java/MyHomeStudy/MyHomeStudy/Square.java
@@ -0,0 +1,17 @@
+package MyHomeStudy.MyHomeStudy;
+
+public class Square {
+    public static int[] calculate(int bound) {
+        int[] rst = new int[bound];
+        for (int i = 0; i < rst.length; i++) {
+            rst[i] = (int) Math.pow(i + 1, 2);
+        }
+        return rst;
+    }
+    public static void main(String[] args) {
+        int[] array = calculate(4);
+        for (int index = 0; index < array.length; index++) {
+            System.out.println(array[index]);
+        }
+    }
+}
---
+++ src/main/java/MyHomeStudy/MyHomeStudy/array/ArrayDefinition.java
@@ -0,0 +1,7 @@
+package MyHomeStudy.MyHomeStudy.array;
+
+public class ArrayDefinition {
+    short [] ages = new short [5];
+    String [] surnames = new String [100500];
+    float [] prices = new float [40];
+}
