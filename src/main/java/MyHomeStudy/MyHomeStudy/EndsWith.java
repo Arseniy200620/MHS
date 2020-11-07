ec8e1a71d0caa16c3c9e0815ce4cc72891e0b6a5
Arseniy200620 <brodnikov2006@gmail.com>
2020-11-07 19:56:54 +0400

    MHS

--- src/main/java/MyHomeStudy/MyHomeStudy/EndsWith.java
+++ src/main/java/MyHomeStudy/MyHomeStudy/EndsWith.java
@@ -4,10 +4,12 @@ public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
         int delta = word.length - post.length;
-        for (int index = post.length - 1; index > -1; index--) {
-            if (word[delta+index] != post[index]) {
-                result = false;
-                break;
+        if (delta > 0) {
+            for (int index = post.length - 1; index > -1; index--) {
+                if (word[delta + index] != post[index]) {
+                    result = false;
+                    break;
+                }
             }
         }
         return result;
