8149d07a6ff4ac03ed102cafefe1d7c07d59bd6c
Arseniy200620 <brodnikov2006@gmail.com>
2020-11-07 11:56:41 +0400

    MHS

--- src/main/java/MyHomeStudy/MyHomeStudy/FindLoop.java
+++ src/main/java/MyHomeStudy/MyHomeStudy/FindLoop.java
@@ -3,7 +3,7 @@ package MyHomeStudy.MyHomeStudy;
 public class FindLoop {
     public static int indexOf(int[] data, int el) {
         int rst = -1; /* если элемента нет в массиве, то возвращаем -1. */
-        for (int index = 0; index < data.length; index++  ) {
+        for (int index = 0; index < data.length; index++) {
             if (data[index] == el) {
                 rst = index;
                 break;
@@ -11,4 +11,19 @@ public class FindLoop {
         }
         return rst;
     }
+
+    public static int index1f(int[] data, int el, int start, int finish) {
+        int rst = -1; /* если элемента нет в массиве, то возвращаем -1. */
+        for (int index = start; index < finish; index++) {
+            if (data[index] == el) {
+                rst = index;
+                break;
+
+            }
+        }
+        return rst;
+    }
 }
+
+
+
